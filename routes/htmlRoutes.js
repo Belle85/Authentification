96f6c273ff0609a864c2342b06e6d85aae2f8902
Belle <bebold.bebelle@gmail.com>
2018-08-25 12:40:30 -0400

    Added passport.js

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,17 +1,26 @@
-var path = require('path');
+var path = require("path");
+
+var isAuthenticated = require('../helpers/isAuthenticated');
 
 module.exports = function (app) {
 
-    app.get("/", function (req, res) {
-        res.sendFile(path.join(__dirname, "../public/signup.html"))
+    app.get("/", (req,res)=>{
+        if(req.user){
+            res.redirect('members')
+        };
+        res.sendFile(path.join(__dirname, "../public/signup.html"));
     });
 
-    app.get("/login", function (req, res) {
-        res.sendFile(path.join(__dirname, "../public/login.html"))
+    app.get("/login", (req,res)=>{
+        if(req.user){
+            res.redirect('members')
+        };
+        res.sendFile(path.join(__dirname, "../public/login.html"));
     });
 
-    app.get("/members", function (req, res) {
-        res.sendFile(path.join(__dirname, "../public/members.html"))
+    app.get("/members",isAuthenticated, (req,res)=>{
+        res.sendFile(path.join(__dirname, "../public/members.html"));
     });
+
 
 }
--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 var express = require("express");
 var bodyParser = require("body-parser");
+var passport = require('./helpers/passport');
+var sessions = require('express-session')
 
 // CONFIG
 
 var PORT = process.env.port || 8080;
 // Using process.env.port is going to allow the app to use any cloud service and if not available it will be 8080
-var mode = process.env.NODE_ENV;
+// var mode = process.env.NODE_ENV;
 var app = express();
 
 // MIDDLEWARE
@@ -14,9 +16,13 @@ app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
 app.use(express.static("public"));
 
+app.use(sessionStorage({secret:"HelloWorld", resave:true, savUninitialized:true}))
+app.use(passport.initialize());
+
 //ROUTES
 
 require('.routes/htmlRoutes')(app); //See the app in the server.js
+require
 
 
 app.listen(PORT, function(){
