5ddf5241ba99af20b257ba08ba66f1900f825573
Belle <bebold.bebelle@gmail.com>
2018-08-25 13:23:41 -0400

    Complete and working authentification application

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 var passport = require('./helpers/passport');
-var sessions = require('express-session')
+var session = require('express-session');
+const db = require("./models");
 
 // CONFIG
+var passport = require('./helpers/passport');
+var secret = require('./config/keys');
+
 
 var PORT = process.env.port || 8080;
 // Using process.env.port is going to allow the app to use any cloud service and if not available it will be 8080
@@ -16,19 +20,21 @@ app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
 app.use(express.static("public"));
 
-app.use(sessionStorage({secret:"HelloWorld", resave:true, savUninitialized:true}))
+app.use(session({ secret: secret.key, resave: true, saveUninitialized: true }));
 app.use(passport.initialize());
+app.use(passport.session());
 
 //ROUTES
 
-require('.routes/htmlRoutes')(app); //See the app in the server.js
-require
+require('./routes/htmlRoutes')(app); //See the app in the server.js
+require('./routes/apiRoutes')(app);
 
-
-app.listen(PORT, function(){
-    // if(mode !== 'production'){
-    //     var opn = require('opn')
-    //     opn(`http://localhost:${PORT}`, {app: ['google chrome']})
-    // }
-    console.log(`Hey I'm listening on ${PORT}`);
-})
+db.sequelize.sync().then(()=>{
+    app.listen(PORT, function () {
+        // if (mode !== 'production') {
+        //     var opn = require('opn');
+        //     opn(`http://localhost:${PORT}`, { app: ['google chrome'] })
+        // }
+        console.log(`👋  Hey there I'm 👂 ing on Port: ${PORT}`);
+    });
+    });
